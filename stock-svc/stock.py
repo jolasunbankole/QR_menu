39958b531fa731c9e0966de1a1ab2c11ebc3a2c7
jolasunbankole <jo2kk9@gmail.com>
2026-09-07 22:13:09 +0100

    Feature[order.py] adding route for html page

--- stock-svc/stock.py
+++ stock-svc/stock.py
@@ -15,7 +15,7 @@ def get_stock():
     return data 
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000)
 
 @app.route('/add-menu', methods=['POST'])
 def add_product():
