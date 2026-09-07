6073ab6c78971ee5781917dde749348482416698
jolasunbankole <jo2kk9@gmail.com>
2026-09-07 23:13:59 +0100

    "Fourth commit"

--- stock-svc/stock.py
+++ stock-svc/stock.py
@@ -4,7 +4,7 @@ from flask import Flask, request
 app = Flask(__name__)
 
 stock_data = {'stock': 
-    {"Coffee": {"Name": "cappuccino", "Price": 3.5},}
+    {"Coffee": {"Name": "cappuccino", "Price": 3.5}}
 }
 
 #route get-stock to return json data of stock_data
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=5000)
 
 @app.route('/add-menu', methods=['POST'])
-def add_product():
+def add_menu():
     # get the product data from the request body
     product = {
         "Name": request.json['Name'],
